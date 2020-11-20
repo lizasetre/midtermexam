e480695a7e03d1a319d2b7a7e785e495bc3415ff
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-11-20 16:10:22 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,80 @@
+<div class="header">
+  <h1> F L O O M </h1>
+    <ul>
+   <li>Shop</li><li>Occasion</li><li>Same Day Delivery</li><li>Get Free Delivery</li><li>Want to sell on floom?</li><li>merch</li>
+   </ul>
+
+   <svg viewBox="0 0 151.2 314.8">
+  <style>
+    .st0{fill:none;stroke:green;stroke-width:7;stroke-miterlimit:10;} .st1{opacity:0.42;fill:green;} .st2{fill:;} .st3{fill:black none;stroke:#3C5B36;stroke-width:0.5;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;} .st4{fill:#F46158;} .st5{fill:#FF9494;} .st6{fill:#F79651;} .st7{fill:#3D3D3D;} .st8{fill:none;stroke:#3D3D3D;stroke-width:0.5;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;} .st9{fill:none;stroke:#3D3D3D;stroke-miterlimit:10;} .st10{fill:none;stroke:#3D3D3D;stroke-width:2;stroke-linecap:circle;stroke-linejoin:circle;stroke-miterlimit:10;} .st11{fill:#EF713A;stroke:#D64C00;stroke-miterlimit:10;}
+  </style>
+  
+<!--   Stem -->
+  <g id="whole-flower">
+  <g id="stem">
+    <path class="st0" d="M80.5 34.8l-1.9 147"/>
+    <path class="st1" d="M76.6 70.4l7 .3-.2 6.2h-7zM75.3 173h7l-.1 2.3h-7z"/>
+    <path class="st2" d="M77.7 114.3s-17.5-19.5-24.5-16 19.5 31 24.5 30.5c5.1-.5 0-14.5 0-14.5z"/>
+    <path class="st2" d="M79.6 114.3s17.5-19.5 24.5-16-19.5 31-24.5 30.5c-5.1-.5 0-14.5 0-14.5z"/>
+    <path class="st3" d="M84.2 118.5l15.5-16.4M95.5 106.6l4.5-.5M90.8 111.4l4.1-.1M95.5 106.6l.5-4.2M90.8 111.6l.1-4.2M86.1 116.8l4.4-.4M85.8 116.7l.9-4.7M73.9 119.4L58.4 103M62.6 107.5l-4.5-.5M67.3 112.3l-4.1-.1M62.6 107.5l-.5-4.2M67.4 112.5l-.1-4.1M72.1 117.8l-4.4-.5M72.3 117.6l-.8-4.7"/>
+  </g>
+    <!--   Flower head -->
+  <g id="flower">
+    <path class="st4" d="M52.7 0l14.7 18.7L80.7 0l10.7 18.7L106 0l10.7 18.7s20 52-32 52c-48 0-58-24-32-70.7z"/>
+    <ellipse class="st5" cx="58.2" cy="41" rx="7.5" ry="3.5"/>
+    <ellipse class="st5" cx="100.9" cy="41.2" rx="7.5" ry="3.5"/>
+    <path class="st6" d="M80.7 0S78.1 12.7 70 17.7c-3.2 2-2.7 1-2.7 1L80.7 0zM106 0s-1.7 10.2-10.2 16.8c-2.7 2.1-4.5 1.8-4.5 1.8L106 0zM52.6 0s.7 4.8-2.2 10.8c-1.4 2.9-3.5 7.3-7.2 9.2 0 0 5.9-15 9.4-20z"/>
+    <circle class="st7" cx="92.2" cy="31.3" r="3.4"/>
+    <path class="st8" d="M87 27.2l4.6 3.8M90.2 25.3l1.2 3.8M97.4 27.2l-3.1 2.4M94.3 25.1l-1.1 3.1"/>
+    <g>
+      <circle class="st7" cx="67.3" cy="31.3" r="3.4"/>
+      <path class="st8" d="M62.1 27.2l4.6 3.8M65.3 25.3l1.2 3.8M72.5 27.2l-3 2.4M69.5 25.1l-1.2 3.1"/>
+    </g>
+    <path class="st10" d="M73.1 47.5s-.6 7.3 7 7.2c7.1 0 6.7-7.2 6.7-7.2"/>
+  </g>
+  </g>
+</svg>
+</div></div>
+
+ <svg xmlns="http://www.w3.org/2000/svg" viewBox="-25 0 150 100" width="150" height="100">
+  <defs>
+    <mask id="mask-petals">
+      <rect width="100" height="100" fill="hsl(0, 0%, 100%)" />
+      <g transform="translate(50 40)">
+        <g class="petals">
+          <circle r="11" fill="hsl(0, 0%, 0%)" />
+        </g>
+      </g>
+    </mask>
+  </defs>
+  <g transform="translate(50 98)">
+    <g class="stem" transform="scale(1 1)">
+      <g fill="currentColor">
+        <g class="leaf" transform="rotate(45)">
+          <path d="M 0 0 c 20 -8 15 -32 0 -42 -15 10 -20 34 0 42" />
+        </g>
+        <g transform="scale(-1 1)">
+          <g class="leaf" transform="rotate(45)">
+            <path d="M 0 0 c 20 -8 15 -32 0 -42 -15 10 -20 34 0 42" />
+          </g>
+        </g>
+      </g>
+    </g>
+  </g>
+  <g mask="url(#mask-petals)">
+    <g transform="translate(50 40)">
+      <g fill="currentColor">
+        <g class="petals">
+          <circle transform="translate(0 -14)" r="11" />
+          <circle transform="rotate(72) translate(0 -14)" r="11" />
+          <circle transform="rotate(144) translate(0 -14)" r="11" />
+          <circle transform="rotate(216) translate(0 -14)" r="11" />
+          <circle transform="rotate(288) translate(0 -14)" r="11" />
+        </g>
+      </g>
+    </g>
+  </g>
+
+  <!-- still unclear about the path accepted by anime.js -->
+  <path id="path" stroke="none" fill="none" d="M -25 -20 a 30 30 0 0 0 60 0 30 30 0 0 0 -60 0 30 30 0 0 1 -60 0 30 30 0 0 1 60 0" />
+</svg>
